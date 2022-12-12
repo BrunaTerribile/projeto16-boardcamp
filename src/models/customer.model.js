6e4e4f746bb10ed1e7f2bb018f48ef06f5b10afe
Bruna Terribile <brunaterribile@gmail.com>
2022-12-12 11:11:59 -0300

    Refact: customerSchema

--- src/models/customer.model.js
+++ src/models/customer.model.js
@@ -2,7 +2,7 @@ import joi from 'joi';
 
 export const customerSchema = joi.object({
     name: joi.string().required(),
-    phone: joi.number().required().min(10).max(11),
-    cpf: joi.number().required().min(11),
-    birthday: joi.number().required()
+    phone: joi.number().min(10),
+    cpf: joi.number().min(11),
+    birthday: joi.required()
 })
