33d5bee3f6552acc71af8489e2944f0f9e76e0ea
Bruna Terribile <brunaterribile@gmail.com>
2022-12-11 23:51:34 -0300

    Feat: postCategory complete (w/ validation)

--- src/controllers/categories.controllers.js
+++ src/controllers/categories.controllers.js
@@ -11,6 +11,24 @@ export async function getCategories(req, res){
     }
 }
 
-export async function postCategory(){
-    
+export async function postCategory(req, res){
+    const { name } = req.body;
+
+    if(!name || name === ""){
+        return res.sendStatus(400)
+    }
+
+    try {
+        const nameExist =  await connection.query("SELECT * FROM categories WHERE name=($1)", [name])
+        if(nameExist){
+            return res.sendStatus(409)
+        }
+
+        const result = await connection.query("INSERT INTO categories (name) VALUES ($1)", [name]);
+        console.log(result);
+        res.sendStatus(201);
+    } catch (err){
+        console.log(err);
+        res.sendStatus(500);
+    }
 }
