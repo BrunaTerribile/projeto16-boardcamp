c71d57b2033ebc67e0d8473b6feb9d1bb83b2f3a
Bruna Terribile <brunaterribile@gmail.com>
2022-12-11 23:49:53 -0300

    Conecting postgres data

--- src/database/db.js
+++ src/database/db.js
@@ -3,6 +3,11 @@ const { Pool } = pkg;
 
 const connection = new Pool({
     connectionString: process.env.DATABASE_URL,
+    host: 'localhost',
+    port: 5432,
+    user: "postgres",
+    password: "driven2022",
+    database: "boardcamp"
 });
 
 export default connection;
